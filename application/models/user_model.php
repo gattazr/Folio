746a3479ff2b2c8fade2e320deda82f828a87534
Rémi GATTAZ <remi.gattaz@free.fr>
2014-02-01 04:24:52 -0800

    Add basic authentication test

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -35,4 +35,21 @@ class User_model extends CI_Model {
         $this->db->insert('user', $this);
     }
 
+    function login($email, $password){
+        $this->db->select('*');
+        $this->db->from('users');
+        $this->db->where('email', $mail);
+        $this->db->where('password', $password);
+        $this->db->limit(1);
+
+        $query = $this->db->get();
+
+        if($query -> num_rows() == 1){
+            return $query->result();
+        }
+        else{
+            return false;
+        }
+ }
+
 }
