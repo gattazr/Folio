e79c2365e3b2d0805a2c13ca50496f71cdd3ea4b
Ma Daniel Thien <mad@quest-OptiPlex-9010>
2014-02-01 09:11:15 -0800

    individual project page db driven now

--- application/views/templates/info_card.php
+++ application/views/templates/info_card.php
@@ -1,5 +1,19 @@
 <?php
 
+function skill_info_card($skill, $colWidth) {
+    echo '<div class="col-sm-';
+    echo $colWidth;
+    echo '">';
+    echo '<div class="panel panel-default">';
+    echo '<div class="panel-heading">';
+    echo '<h3 class="panel-title">';
+    echo $skill['name'];
+    echo '</h3></div>';
+    echo '<div class="panel-body">';
+    echo $skill['level'];
+    echo '</div></div></div>';
+}
+
 function project_info_card($project, $colWidth) {
     //colWidth is 4 for welcome page, 3 everywhere else
     echo '<div class="col-sm-';
@@ -16,6 +30,7 @@ function project_info_card($project, $colWidth) {
     echo $project['logo'];
     echo '"/></div>';
     echo '<div class="panel-text">';
+    echo 'By Someone';
     echo $project['description'];
     echo '</div></div></div></div>';
 }
