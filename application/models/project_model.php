8d6467ff3aab4ba312ed85ddad513aa1a42c442a
Paul McEntyre Croft <croftp2@quest-OptiPlex-9010>
2014-02-01 00:22:02 -0800

    updated project model. Assumed reading from post files

--- application/models/project_model.php
+++ application/models/project_model.php
@@ -23,7 +23,58 @@ class project_model extends CI_Model {
 
         $query = $this->db->get();
 
-        return $query
+        return $query;
+    }
+
+
+    function get_one(){
+
+
+        $this->db->select('id,owner,name,description,logo,startdate,enddate');
+        $this->db->from('project');
+        $this->db->limit('1');
+
+        $query = $this->db->get();
+
+        return $query;
+    }
+
+
+    function insert_entry() {
+
+        
+        $this->id = $this->input->post('owner_id');
+        $this->owner = $this->input->post('owner');
+        $this->name = $this->input->post('name');
+        $this->description = $this->input->post('description');
+        $this->logo = $this->input->post('logo');
+        $this->startdate = $this->input->post('startdate');
+        $this->enddate = $this->input->post('enddate');
+
+        $this->db->insert('user', $this);
+    }
+
+    function update_entry() {
+        
+        $this->id = $this->input->post('id');
+
+        $data = array(
+            'owner' = $this->input->post('owner'),
+            'name' = $this->input->post('name'),
+            'description' = $this->input->post('description'),
+            'logo' = $this->input->post('logo'),
+            'startdate' = $this->input->post('startdate'),
+            'enddate' = $this->input->post('enddate'),
+        );
+
+        $this->db->where('id', $this->id);
+        $this->db->update('mytable', $data);
+
+    }
+
+    function remove_entry(){
+
+        NULL;
     }
 
 }
