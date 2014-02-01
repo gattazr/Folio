95ea739619b69a5239a1a49aa0554f276cec54bf
Paul McEntyre Croft <croftp2@quest-OptiPlex-9010>
2014-02-01 05:49:59 -0800

    Initial upload of the pskill model database tools

---
+++ application/models/pskill_model.php
@@ -0,0 +1,91 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class pskill_model extends CI_Model {
+
+    var $owner = 0;
+    var $name = '';
+    var $level = '';
+
+    function __construct() {
+        parent::__construct();
+    }
+
+
+    function get_entry(){
+//        $this->owner_id = $this->input->post('owner_id');
+        $this->owner = '3';
+
+        $this->db->select('name,level');
+        $this->db->where('owner',$this->owner);
+
+        $query = $this->db->get('pskill')->result_array();
+
+        return $query;
+    }
+
+
+    function insert_entry() {
+        /*
+        $this->owner_id = $this->input->post('owner_id');
+        $this->name = $this->input->post('name');
+        $this->level = $this->input->post('level');
+        */
+
+        $this->owner = '3';
+        $this->name = 'basket weaving';
+        $this->level = 'PhD+';
+
+        $this->db->insert('pskill', $this);
+    }
+
+    function remove_entry(){
+        /*
+        $this->owner_id = $this->input->post('owner_id');
+        $this->name = $this->input->post('name');
+        */
+
+/*
+        $this->owner_id = 'dev_croftp2username';
+        $this->name = 'dev_croftp2firstname';
+        
+        $this->db->delete('skill', $this);
+*/
+        NULL;
+    }
+
+    function modify_entry(){
+        /*
+        $this->owner_id = $this->input->post('owner_id');
+        $this->name = $this->input->post('name');
+        */
+
+        $this->owner = '3';
+        $this->name = 'basket weaving';
+
+        $data = array(
+            /*
+            'name' => $this->input->post('name'),
+            'level' => $this->input->post('level')
+            */
+            'name' => 'writing code',
+            'level' => 'the best'
+        );
+
+        $this->db->where('owner', $this->owner);
+        $this->db->where('name', $this->name);
+        $this->db->update('pskill', $data);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
