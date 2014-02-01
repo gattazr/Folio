bf5f96b89551447d8b773bda0b4919bd1c06e170
Rémi GATTAZ <remi.gattaz@free.fr>
2014-01-31 21:41:59 -0800

    Create template functions Add an view called example

---
+++ application/views/example.php
@@ -0,0 +1,24 @@
+<?php 
+include ('templates/page_header.php');
+include ('templates/headmenu.php');
+?>
+
+<html>
+	<?php
+	open_page_header('template page');
+	close_page_header();?>
+	<body>
+		<?php headmenu(); ?>
+		<div class="container theme-showcase" role="main">
+
+			<div class="jumbotron">
+				<h1>Template test page</h1>
+				<p>This is a simple example of Jumbotron</p>
+			</div>
+
+			<div class="page-header">
+        		<h2>Template test page</h2>
+			</div>
+		</div>
+	</body>
+<html>
