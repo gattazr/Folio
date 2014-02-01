ec75aa74eae533513e3ff085f519a79625e67060
Paul McEntyre Croft <croftp2@quest-OptiPlex-9010>
2014-02-01 03:47:14 -0800

    Giving user_model insert enrty, update entry, find_entry

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -11,9 +11,21 @@ class User_model extends CI_Model {
     var $city = '';
     var $country = '';
     var $avatar = '';
+    var $password = '';
 
     function __construct() {
         parent::__construct();
+    }
+
+    function find_entry(){
+//        $this->username = $this->input->post('username');
+        $this->username = 'devEusername';
+
+        $this->db->select('id, username, firstname, lastname, city, country, avatar, password, email');
+        $this->db->where('username',$this->username);
+        $query = $this->db->get('user')->result_array();
+
+        return $query;
     }
 
     function insert_entry() {
@@ -24,15 +36,56 @@ class User_model extends CI_Model {
         $this->country = $this->input->post('country');
         $this->avatar = $this->input->post('avatar'):*/
 
-       
-        $this->username = 'devDusername';
-        $this->firstname = 'devDfirstname';
-        $this->lastname = 'devDlastname';
-        $this->city = 'devDcity';
-        $this->country = 'devDcountry';
-        $this->avatar = 'devDavatar';
-       
+        $this->id = 0;
+        $this->username = 'devEusername';
+        $this->firstname = 'devEfirstname';
+        $this->lastname = 'devElastname';
+        $this->city = 'devEcity';
+        $this->country = 'devEcountry';
+        $this->avatar = 'devEavatar';
+        $this->password = 'devEpassword';
+        $this->email = 'devEemail';
+
         $this->db->insert('user', $this);
     }
+
+    function update_entry() {
+        
+//        $this->username = $this->input->post('username');
+        $this->username = 'devEusername';
+
+        $data = array(
+/*
+            'username' => $this->input->post('username'),
+            'firstname' => $this->input->post('firstname'),
+            'lastname' => $this->input->post('lastname'),
+            'city' => $this->input->post('city'),
+            'country' => $this->input->post('country'),
+            'avatar' => $this->input->post('avatar'),
+            'password' => $this->input->post('password'),
+            'email' => $this->input->post('email')
+*/
+            'username' => 'username',
+            'firstname' => 'firstname',
+            'lastname' => 'lastname',
+            'city' => 'city',
+            'country' => 'country',
+            'avatar' => 'avatar',
+            'password' => 'password',
+            'email' => 'email'
+        );
+
+        $this->db->where('username', $this->username);
+        $this->db->update('user', $data);
+
+    }
+
+    function remove_entry(){
+
+        NULL;
+    }
+
+
+
 
 }
